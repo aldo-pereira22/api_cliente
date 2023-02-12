862f717a0816c02b7ecc3dc5eeca060333f7ec30
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-02-12 11:24:59 -0300

    Padronização das mensagens de erro

---
+++ apiCliente/src/main/java/ws/dto/error/ErrorResponseDto.java
@@ -0,0 +1,18 @@
+package ws.dto.error;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ErrorResponseDto {
+	
+	private String message;
+	private HttpStatus httpStatus;
+	private Integer statusCode;
+	
+}
